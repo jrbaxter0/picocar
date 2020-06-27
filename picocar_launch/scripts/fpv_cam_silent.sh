024db18afc9224264a4ed76a86249043be9f31d5
James Baxter <baxterjr@outlook.com>
2020-06-27 14:47:32 -0400

    switch to cv_camera

--- picocar_launch/scripts/fpv_cam_silent.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-# hide all warnings regarding deprecated pixel format
-# will also hide any important wornings, so keep that in mind
-roslaunch picocar_launch fpv_cam.launch 2>/dev/null
